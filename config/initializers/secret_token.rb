e486a7277db3956a048de27ff2eb098d255dec7a
Al Rivera <your.prettyuglyproductions@gmail.com>
2012-07-18 22:21:40 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheers::Application.config.secret_token = '994eafa19e10da1a66b61ed47fd86ceb1690aa607a88066e2be344c26f619f6c20364f300eeacd50b38a20a5065b6305fd4792cfe5bee7e8a463bcf3d5bb53af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cheers::Application.config.session_store :cookie_store, key: '_cheers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cheers::Application.config.session_store :active_record_store
